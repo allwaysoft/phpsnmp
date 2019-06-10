f1c6a1ca12519f919fee2658ee6c795194e40fe2
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2019-06-10 13:20:09 +0800

    Add files via upload

---
+++ snmptable4.php
@@ -0,0 +1,20 @@
+<?php
+snmp_set_quick_print(true);
+$host = "127.0.0.1";
+$community = "public";
+$oid = "1.3.6.1.2.1.25.4.2";
+$arr = snmptable($host, $community, $oid, 7);
+for ($i=1; $i<=count($arr); $i++)
+     echo "{$arr[$i][1]}\t{$arr[$i][2]}\t{$arr[$i][3]}\t{$arr[$i][4]}\t{$arr[$i][5]}\t{$arr[$i][6]}\t{$arr[$i][7]}\n";
+
+function snmptable($host, $comm, $oid, $numCols) {
+	for ($i=1;$i<=$numCols;$i++)
+		$ret[$i] = snmpwalk($host, $comm, "$oid.1.$i");
+	$numRows = count($ret[1]);
+	for ($i=1; $i<=$numRows; $i++) {
+		for ($j=1;$j<=$numCols;$j++) 
+			$table[$i][$j]= $ret[$j][$i-1];
+	}
+	return $table;
+}
+?>
